2e6e325b85e710dc0073bb77cf622970cf329d2f
Tobias Leinss <caruso33@web.de>
2022-08-31 11:44:19 +0200

    Fix var naming

--- src/index.ts
+++ src/index.ts
@@ -2,17 +2,17 @@ import euler_1 from "./euler_1"
 import euler_10 from "./euler_10"
 
 function main() {
-  let sumOfMultiplies, threshold
+  let threshold
 
   threshold = 1000
 
-  sumOfMultiplies = euler_1(threshold)
+  const sumOfMultiplies = euler_1(threshold)
   console.log(`The solution for euler_1 is ${sumOfMultiplies}`)
 
   threshold = 2 * 1e6
 
-  sumOfMultiplies = euler_10(threshold)
-  console.log(`The solution for euler_10 is ${sumOfMultiplies}`)
+  const sumOfPrimes = euler_10(threshold)
+  console.log(`The solution for euler_10 is ${sumOfPrimes}`)
 }
 
 main()
--- tests/euler_10.ts
+++ tests/euler_10.ts
@@ -17,18 +17,18 @@ describe("Euler 10", () => {
   it("finds sum of all primes which are below 10", () => {
     const threshold = 10
 
-    const sumOfMultiplies = euler_10(threshold)
+    const sumOfPrimes = euler_10(threshold)
 
     assert(
-      sumOfMultiplies == 17,
-      `Wrong result provided. Logic incorrect. Is ${sumOfMultiplies} should be 17.`
+      sumOfPrimes == 17,
+      `Wrong result provided. Logic incorrect. Is ${sumOfPrimes} should be 17.`
     )
   })
 
   it("finds sum of all primes which are below 2mil", () => {
     const threshold = 2 * 1e6
 
-    const sumOfMultiplies = euler_10(threshold)
-    console.log(`The solution for euler_10 is ${sumOfMultiplies}`)
+    const sumOfPrimes = euler_10(threshold)
+    console.log(`The solution for euler_10 is ${sumOfPrimes}`)
   })
 })
